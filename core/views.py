cde8df16e5fcbfe6aa24f1c1cd4834d7ad62ac57
Abram-MrRight <atongkurabraham@gmail.com>
2023-08-08 09:21:34 +0300

    Updated navigation bar design

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from location.models import Location
 from django.contrib.auth.decorators import login_required
 from .forms import *
-import folium, geocoder
+import folium, geocoder 
 from geopy.geocoders import Nominatim
 
 # Create your views here.
